1cc0ef70c7ce9c083fa743f913f0a2419641ef7c
TharinduDR <rhtdranasinghe@gmail.com>
2020-04-03 11:16:01 +0100

    001: Initial commit

--- run_experiment.py
+++ run_experiment.py
@@ -3,11 +3,15 @@ from sklearn.metrics import mean_absolute_error
 import pandas as pd
 from config.transformer_config import transformer_config, MODEL_TYPE, MODEL_NAME
 from evaluation import pearson_corr, spearman_corr
+from normalizer import fit
 from transformer_model import TrnsformerModel
 
 
 train = pd.read_csv("data/stsbenchmark/sts-train-dev.csv", usecols=[4, 5, 6], names=['labels', 'text_a', 'text_b'])
 eval_df = pd.read_csv("data/stsbenchmark/sts-test.csv", usecols=[4, 5, 6], names=['labels', 'text_a', 'text_b'])
+
+train = fit(train, "labels")
+eval_df = fit(eval_df, "labels")
 
 model = TrnsformerModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
                            args=transformer_config)
